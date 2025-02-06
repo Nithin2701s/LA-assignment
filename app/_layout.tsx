06115123a3e754a0bd033d55bed429c8a9af23f2
Nithinsai2701 <nithincns2701@gmail.com>
2025-02-06 19:26:59 +0530

    Add tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Text, useColorScheme, View } from 'react-native'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
+const RootLayout = () => {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+    <ThemeProvider value={colorScheme === 'dark'? DarkTheme:DefaultTheme} >
+       <View style={{height:35}}>
+       </View>
+     <Stack>
+      <Stack.Screen name='(tabs)' options={{headerShown:false}}  />
+      <Stack.Screen name='+not-found' />
+     </Stack>
+     <StatusBar style="auto" />
     </ThemeProvider>
-  );
+  )
 }
+
+export default RootLayout
---
+++ components/TabIconSymbol.tsx
@@ -0,0 +1,11 @@
+import { MaterialIcons } from "@expo/vector-icons"
+
+
+const TabIconSymbol = ({name,size,color})=>{
+
+   return (
+    <MaterialIcons name={name} size={size} color={color} />
+   )
+}
+
+export default TabIconSymbol
